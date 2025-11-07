931472f0109fd69530f930247435c98ead1818ae
FishingFella <kieran.hodgson86@gmail.com>
2025-11-07 01:13:08 +0000

    Commentation
    Explanation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,20 +25,26 @@ public class PlayerMovement : MonoBehaviour
 
     void OnMove(InputValue movement)
     {
+        //basically x-axis is horizontal (A and D) movement and y is vertical (W and S)
         Vector2 movementVector = movement.Get<Vector2>();
 
-        if (movementVector.x > 0)
+        if (movementVector.x > 0)//above 0 is right
         {
+            //calculate where we want to be with current input
             Vector3 desiredPosition = transform.position + new Vector3(offset, 0, 0);
+            //calculate the maximum point we can be at
+            //possible optimisation by defining this at the start
             float maxX = _startingPosition.x + (offset * additionalSideLanes);
 
-            if (desiredPosition.x <= maxX)
+            if (desiredPosition.x <= maxX) //if desired point is equal to or below or maximum then
             {
+                //move the player by the offset
                 transform.position += new Vector3(offset, 0, 0);   
             }
             
-        }else if (movementVector.x < 0)
+        }else if (movementVector.x < 0)// below 0 is left
         {
+            //same as above but for negative
             Vector3 desiredPosition = transform.position - new Vector3(offset, 0, 0);
             float maxX = _startingPosition.x - (offset * additionalSideLanes);
 
