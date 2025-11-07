4d63836b9f6777887144c55a99782cad59ea32b0
FishingFella <kieran.hodgson86@gmail.com>
2025-11-07 00:15:39 +0000

    Basic Movement
    No lanes
    Basic
    Needs improvement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float offset; //how much the player will move to get to the next lane
+    public int maxLanes; //must be a multiple of 3
+    
+    private Vector3 _startingPosition;
+    private Vector3 _maxLaneLeft;
+    private Vector3 _maxLaneRight;
+    private float _movementX;
+    
     void Start()
     {
+        //Ignore this
+        //go left. and make right of left the middle. right of middle is not left, it is the right.
+        //Ignore this
         
-    }
+        _startingPosition =  transform.position;
 
-    // Update is called once per frame
+        maxLanes /= 3;
+        
+        _maxLaneLeft = transform.position + new Vector3(offset*maxLanes, 0, 0);
+        _maxLaneRight = transform.position - new Vector3(offset*maxLanes, 0, 0);
+    }
+    
     void Update()
     {
-        
+        Vector3 desiredPosition = transform.position + new Vector3(offset * _movementX, 0, 0);
+        if (desiredPosition != transform.position)
+        {
+            
+        }
+    }
+
+    void OnMove(InputValue movement)
+    {
+        Vector2 movementVector = movement.Get<Vector2>();
+
+        if (movementVector.x > 0)
+        {
+            _movementX = 1;
+        }else if (movementVector.x < 0)
+        {
+            _movementX = -1;
+        }
+        else
+        {
+            _movementX = 0;
+        }
     }
 }
